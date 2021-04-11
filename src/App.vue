6db7ce0b5bf10bbf0a1d29ab25ed09968a162e34
Moon Quddus <moonquddus@gmail.com>
2021-04-11 15:38:10 +0100

    Added data table

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,13 @@
     <div class="container">
       <h1 class="title is-1">Cardiff &amp; Vale - Vaccination Progress</h1>
       <VaccineGraph v-bind:chartData="chartData" />
-      <h2 class="title is-2">Data</h2>
+      <VaccineTable v-bind:chartData="chartData" />
     </div>
   </section>
   <footer class="footer">
     <div class="content has-text-centered">
       <p>
-        Made by <a href="https://linktr.ee/moon.quddus">Moon Quddus</a>. The source code is licensed
+        Made by <a target="_blank" href="https://linktr.ee/moon.quddus">Moon Quddus</a>. The <a target="_blank" href="https://github.com/moonquddus/cardiff-vaccine-progress">source code</a> is licensed
         <a target="_blank" rel="nofollow" href="https://www.gnu.org/licenses/gpl-3.0.en.html">GNU General Public License (GPL), version 3</a>.
       </p>
     </div>
@@ -19,6 +19,7 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import VaccineGraph from './components/VaccineGraph.vue'
+import VaccineTable from './components/VaccineTable.vue'
 import vaccineData from './data'
 import 'bulma/css/bulma.css'
 // import './assets/bulmaswatch.min.css'
@@ -26,7 +27,8 @@ import 'bulma/css/bulma.css'
 export default defineComponent({
   name: 'App',
   components: {
-    VaccineGraph
+    VaccineGraph,
+    VaccineTable
   },
   data: function() {
     return {
--- src/components/VaccineTable.vue
+++ src/components/VaccineTable.vue
@@ -1,13 +1,77 @@
 <template>
     <h2 class="title is-2">Data</h2>
-    <table class="table is-striped is-hoverable">
-        <thead>
+    <div class="table-container">
+      <table class="table is-striped is-hoverable">
+          <thead>
             <tr>
-                <th>Date</th>
-                <th>1st Vaccinations</th>
-                <th>2nd Vaccinations</th>
+              <th>Date</th>
+              <th class="has-text-centered">1st vaccinations (%)</th>
+              <th class="has-text-centered">New 1st doses</th>
+              <th class="has-text-centered">2nd vaccinations (%)</th>
+              <th class="has-text-centered">New 2nd doses</th>
             </tr>
-        </thead>
-        <tbody></tbody>
-    </table>
+          </thead>
+          <tbody>
+            <tr v-for="(dataPoint, index) in sortedData" :key="index">
+              <td><strong>{{ new Date(dataPoint.label).toLocaleDateString('en-GB', {dateStyle: 'long'}) }}</strong></td>
+              <td class="has-text-centered">{{ dataPoint.value[0] }} <em class="is-size-7">({{ calculatePercentage(dataPoint.value[0]) }}%)</em></td>
+              <td class="has-text-centered">{{ getValueDifference(index, 0) }}</td>
+              <td class="has-text-centered">{{ dataPoint.value[1] }} <em class="is-size-7">({{ calculatePercentage(dataPoint.value[1]) }}%)</em></td>
+              <td class="has-text-centered">{{ getValueDifference(index, 1) }}</td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <th>Date</th>
+              <th class="has-text-centered">1st Vaccinations</th>
+              <th class="has-text-centered">%</th>
+              <th class="has-text-centered">2nd Vaccinations</th>
+              <th class="has-text-centered">%</th>
+            </tr>
+          </tfoot>
+      </table>
+    </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+
+export interface ChartDataPoint {
+  label: string,
+  value: Array<string>
+}
+
+export default defineComponent({
+  name: 'VaccineTable',
+  props: {
+    chartData: {
+      type: Array as PropType<Array<ChartDataPoint>>,
+      required: true
+    },
+  },
+  data: function() {
+    return {
+      population: 133587 + 366903 // cardiff + vale
+    }
+  },
+  computed: {
+    sortedData: function(): Array<ChartDataPoint>{
+      return this.chartData.reverse()
+    }
+  },
+  methods: {
+    getNumber: function(input: string): number {
+      return ~~input.replace(',','')
+    },
+    calculatePercentage: function(value: string): number {
+      return Math.round((this.getNumber(value) / this.population) * 1000) / 10
+    },
+    getValueDifference: function(index: number, valueIndex: number): number {
+      return this.sortedData[index + 1] ? this.getNumber(this.sortedData[index].value[valueIndex]) - this.getNumber(this.sortedData[index + 1].value[valueIndex]) : this.getNumber(this.sortedData[index].value[valueIndex])
+    }
+  }
+})
+</script>
+
+<style scoped>
+</style>
