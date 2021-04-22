f348639cd0e2464557b4741c4e2383293b64e1a4
Moon Quddus <moonquddus@gmail.com>
2021-04-22 09:24:01 +0100

    lint

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/cardiff-vaccine-progress/',
-  plugins: [vue()]
+  plugins: [
+    vue()
+  ]
 })
